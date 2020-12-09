89adc105414eccac159be1ddfe8e1ddb798c1285
Michael Forney <mforney@mforney.org>
2020-12-09 13:45:13 -0800

    Add some comments and minor tweaks

--- resample.c
+++ resample.c
@@ -43,6 +43,7 @@ resample_eof(struct resampler *r)
 	r->in_stride = 0;
 }
 
+/* evaluate the filter at time t, given in 16:16 fixed-point */
 static inline int16_t
 h(unsigned long t)
 {
@@ -53,14 +54,17 @@ h(unsigned long t)
 size_t
 resample(struct resampler *r)
 {
-	unsigned long f_in = r->in_rate, f_out = r->out_rate;
-	unsigned long cutoff = f_in < f_out ? 0x10000 : f_out * 0x10000ull / f_in;
-	unsigned long t = r->t, t_in = L << 16, t_out = (unsigned long long)t_in * f_in / f_out;
-	unsigned long p, dt = t_in * cutoff >> 16, t_max = N * t_in;
+	/* resample ratio, Fout/Fin, as 16:16 fixed-point */
+	unsigned long ratio = r->out_rate * 0x10000ull / r->in_rate;
+	/* low-pass filter cutoff; 1 when upsampling, Fout/Fin when downsampling */
+	unsigned long cutoff = ratio > 0x10000 ? 0x10000 : ratio;
+	/* current time, input period, and output period */
+	unsigned long t = r->t, t_in = L << 16, t_out = ((unsigned long long)t_in << 16) / ratio;
+	/* filter position, step size, and maximum */
+	unsigned long p, p_0, dp = L * cutoff, p_max = N * t_in;
 	short *x = r->x, *end = r->buf + 3 * B + 1;
-	size_t n = 0;
+	size_t n = 0, i;
 	long y;
-	int i;
 
 	for (;;) {
 		for (; t >= t_in; t -= t_in, --r->in_frames) {
@@ -76,12 +80,16 @@ resample(struct resampler *r)
 			if (r->out_frames == 0)
 				goto done;
 			y = 0;
-			for (i = 0, p = t * cutoff >> 16; i <= B && p <= t_max; ++i, p += dt)
+			p_0 = (unsigned long long)t * cutoff >> 16;
+			/* left side of filter */
+			for (i = 0, p = p_0; i <= B && p <= p_max; ++i, p += dp)
 				y += x[-i] * h(p) >> 7;
-			for (i = 1, p = (t_in - t) * cutoff >> 16; i <= B && p <= t_max; ++i, p += dt)
+			/* right side of filter */
+			for (i = 1, p = dp - p_0; i <= B && p <= p_max; ++i, p += dp)
 				y += x[i] * h(p) >> 7;
-			/* shift back and clamp to int16 range */
-			y = (((y + 0x80) >> 8) * (long)cutoff + 0x8000) >> 16;
+			/* scale back to int16 range */
+			y = (((y + 0x80ll) >> 8) * (long)cutoff + 0x8000) >> 16;
+			/* clamp to int16 range, if necessary */
 			*r->out = y > 0x7fff ? 0x7fff : y < -0x8000 ? -0x8000 : y;
 			r->out += r->out_stride;
 			++n;
